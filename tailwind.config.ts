1f553b00812fc6f99282c89fc60abb621b11d89b
Shubham Mittal <shubham@tinyemail.com>
2024-03-09 15:38:30 +0530

    Added twitter logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'hover-background': 'rgba(239, 243, 244, 0.1)'
+      }
     },
   },
   plugins: [],
