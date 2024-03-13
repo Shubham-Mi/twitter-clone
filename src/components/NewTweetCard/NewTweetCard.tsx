24bc3e2a53d2411a34e614ea0882a1a51d836b1b
Shubham Mittal <shubham.mitt112@gmail.com>
2024-03-14 02:39:48 +0530

    Added select image to new tweet popup

--- src/components/NewTweetCard/NewTweetCard.tsx
+++ src/components/NewTweetCard/NewTweetCard.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
-import React from "react";
+import React, { useCallback } from "react";
 import { useCurrentUser } from "../../../hooks/user";
 import { FaImage } from "react-icons/fa6";
 
 export default function NewTweetCard() {
   const { user } = useCurrentUser();
+
+  const handleImageSelect = useCallback(() => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+  }, []);
 
   return (
     <div>
@@ -33,7 +40,7 @@ export default function NewTweetCard() {
           ></textarea>
           <div className="flex justify-between pr-10 mt-4 items-center">
             <div>
-              <FaImage className="text-xl" />
+              <FaImage onClick={handleImageSelect} className="text-xl" />
             </div>
             <div>
               <button className="bg-primary-color hover:bg-primary-color-hover rounded-full px-4 py-2 w-fit font-bold transition-all">
