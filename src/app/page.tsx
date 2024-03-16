b951fba7f0da5dc7f38349b742ee6f256f521901
Shubham Mittal <shubham.mitt112@gmail.com>
2024-03-16 22:28:29 +0530

    refactored code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 "use client";
 import React from "react";
-import Home from "./Home";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Toaster } from "react-hot-toast";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useGetAllTweets } from "../../hooks/tweet";
+import NewTweetCard from "@/components/NewTweetCard";
+import FeedCard from "@/components/FeedCard";
+import { Tweet } from "../../gql/graphql";
 
-const queryClient = new QueryClient();
+export default function Page() {
+  const { tweets } = useGetAllTweets();
 
-export default function App() {
   return (
     <div>
-      <QueryClientProvider client={queryClient}>
-        <GoogleOAuthProvider clientId="8232000391-19qf54pm6v1faf8igjk0sagmjnusd2p1.apps.googleusercontent.com">
-          <Home />
-          <Toaster />
-          <ReactQueryDevtools />
-        </GoogleOAuthProvider>
-      </QueryClientProvider>
+      <NewTweetCard />
+      {tweets?.map((tweet) =>
+        tweet ? <FeedCard key={tweet.id} data={tweet as Tweet} /> : null
+      )}
     </div>
   );
 }
---
+++ src/app/template.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Sidebar from "@/components/sidebar";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <GoogleOAuthProvider clientId="8232000391-19qf54pm6v1faf8igjk0sagmjnusd2p1.apps.googleusercontent.com">
+          <div className="grid grid-cols-12 h-screen w-screen px-14">
+            <div className="col-span-3">
+              <Sidebar />
+            </div>
+            <div className="col-span-6 border-x-[1px] border-border-color h-screen overflow-auto">
+              {children}
+            </div>
+            <div className="col-span-3"></div>
+          </div>
+          <Toaster />
+          <ReactQueryDevtools />
+        </GoogleOAuthProvider>
+      </QueryClientProvider>
+    </div>
+  );
+}
