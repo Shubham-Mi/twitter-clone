d2c4acafe6f5ed5db2d6de6be6b2d2e0ca7f373e
Shubham Mittal <shubham.mitt112@gmail.com>
2024-03-23 18:14:52 +0530

    show follow and unfollow button

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { FaArrowLeft } from "react-icons/fa6";
-import { useUserById } from "../../../hooks/user";
+import { useCurrentUser, useUserById } from "../../../hooks/user";
 import Image from "next/image";
 import FeedCard from "@/components/FeedCard";
 import { Tweet } from "../../../gql/graphql";
+import { useMemo } from "react";
 
 export default function Page({ params }: { params: { userId: string } }) {
   const { user } = useUserById(params.userId);
+  const { user: currentUser } = useCurrentUser();
+
+  const isFollowing = useMemo(() => {
+    if (!currentUser) return false;
+    return (
+      (currentUser.following?.findIndex((el) => el?.id === params.userId) ??
+        -1) >= 0
+    );
+  }, [currentUser, params.userId]);
 
   return (
     <div>
@@ -35,18 +45,37 @@ export default function Page({ params }: { params: { userId: string } }) {
           height={100}
           className="rounded-full"
         />
-        <h1 className="text-2xl font-bold mt-5">
-          {user?.firstName} {user?.lastName}
-        </h1>
-        <div className="flex gap-3">
-          <h1 className="text-md text-slate-400 flex gap-1">
-            <span className="text-white">{user?.following?.length}</span>
-            <span>Following</span>
-          </h1>
-          <h1 className="text-md text-slate-400 flex gap-1">
-            <span className="text-white">{user?.followers?.length}</span>
-            <span>Followers</span>
-          </h1>
+        <div className="flex justify-between mt-5 mx-2 items-center">
+          <div>
+            <h1 className="text-2xl font-bold">
+              {user?.firstName} {user?.lastName}
+            </h1>
+            <div className="flex gap-3">
+              <h1 className="text-md text-slate-400 flex gap-1">
+                <span className="text-white">{user?.following?.length}</span>
+                <span>Following</span>
+              </h1>
+              <h1 className="text-md text-slate-400 flex gap-1">
+                <span className="text-white">{user?.followers?.length}</span>
+                <span>Followers</span>
+              </h1>
+            </div>
+          </div>
+          <div>
+            {user?.id !== currentUser?.id && (
+              <>
+                {isFollowing ? (
+                  <button className="bg-white text-black hover:bg-gray-300 rounded-full px-4 py-2 w-fit font-bold transition-all">
+                    Unfollow
+                  </button>
+                ) : (
+                  <button className="bg-white text-black hover:bg-gray-300 rounded-full px-4 py-2 w-fit font-bold transition-all">
+                    Follow
+                  </button>
+                )}
+              </>
+            )}
+          </div>
         </div>
       </div>
       <div>
